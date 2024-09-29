bad41067aec2d5bf857fe2a8e5e12a57cc737c87
richie.p <mohammadpagard.dev@gmail.com>
2024-09-29 21:24:00 +0330

    implement -Implemented the Stack Data Structure.

---
+++ data-structure/stack/stack.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "stack.h"
+
+
+Stack *initialStack(int length) {
+    Stack *st;
+
+    st = (Stack*) malloc(sizeof(Stack));
+    st->items = (int*) malloc(length * sizeof(int));    // allocate memory for the items array
+    st->length = length;
+    st->top = -1;
+
+    return st;
+}
+
+void freeStack(Stack *st) {
+    /* Free the items array and the stack structure itself */
+    free(st->items);
+    free(st);
+}
+
+int isFull(Stack *st) {
+    if(st->top == 9) return 1;
+    else return 0;
+}
+
+int isEmpty(Stack *st) {
+    if(st->top == -1) return 1;
+    else return 0;
+}
+
+void push(Stack *st) {
+    int NUMBER_INPUT;
+    printf("Enter a number to push on the Stack: ");
+    scanf("%d", &NUMBER_INPUT);
+
+    if(isFull(st)) printf("The Stack is full...\n");
+    else
+    {
+        st->top++;
+        st->items[st->top] = NUMBER_INPUT;
+    }
+}
+
+int pop(Stack *st) {
+	int item;
+
+    if(isEmpty(st)) printf("\nThe Stack is empty...\n");
+    else
+	{
+		item = st->items[st->top];
+		st->top--;
+
+	}
+	return item;
+}
