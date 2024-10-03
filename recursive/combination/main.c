587ce0226a8a312d2bd060a882983ae9f5b126d4
richie.p <mohammadpagard.dev@gmail.com>
2024-10-03 21:50:49 +0330

    implement -Implemented the Recursive Combination Formula.

---
+++ recursive/combination/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int recursiveCombination(int n, int m);
+
+
+int main() {
+	printf("%d\n", recursiveCombination(5, 3));
+
+	return 0;
+}
+
+int recursiveCombination(int n, int m) {
+	/*
+	    Binomial Coefficient Formula:
+
+           n!
+	    C(n, m) = --------
+            m! (n - m)!
+
+		Where:
+        - n! is the factorial of n
+        - m! is the factorial of m
+        - (n - m)! is the factorial of (n - m)
+
+		This formula is used to compute the number of ways to choose 'm' elements from a set of 'n' elements.
+
+
+		Recursive Combination Formula:
+
+		C(n, m) = C(n-1, m) + C(n-1, m-1)
+
+		Where:
+			- C(n, m) is the number of ways to choose 'm' elements from a set of 'n' elements.
+			- The base cases are:
+				* C(n, 0) = 1 (There is only one way to choose 0 elements)
+				* C(n, n) = 1 (There is only one way to choose all 'n' elements)
+
+		This recursive formula breaks down the problem into smaller subproblems by reducing 'n' and/or 'm' until the base case is reached.
+	 */
+
+	if( (n == m) || (m == 0) ) return 1;
+	else
+	{
+		return recursiveCombination(n-1, m) + recursiveCombination(n-1, m-1);
+	}
+
+}
+
