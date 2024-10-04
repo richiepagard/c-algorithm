a3219e82ef652b4b61cc49d68a95d8b260978d54
richie.p <mohammadpagard.dev@gmail.com>
2024-10-04 12:32:44 +0330

    implement -Implemented the Recursive Ackerman Function .

---
+++ recursive/ackermann/main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int recursiveAckerman(int n, int m);
+
+
+int main() {
+	printf("\n");
+	printf("%d\t", recursiveAckerman(1, 1));
+	printf("%d\t", recursiveAckerman(2, 5));
+	printf("%d\t", recursiveAckerman(3, 2));
+	printf("%d\n\n", recursiveAckerman(4, 1));
+
+	return 0;
+}
+
+
+int recursiveAckerman(int n, int m) {
+	/*
+		Recursive Ackerman Formula:
+		The Ackermann function is a recursive function with deep theoretical significance due to its fast-growing nature.
+
+		It is defined as:
+
+			A(n, m) = 
+				- m + 1								if n = 0
+				- A(n - 1, 1)						if m = 0
+				- A( n - 1, A(n, m - 1) )			if n > 0 and m > 0
+		
+		Explanation:
+			- A(n, m) takes two non-negative integers 'n' and 'm'.
+			- It is a deeply recursive function, where each recursive call reduces 'n' and/or 'm' until the base case is reached.
+
+		Base Case:
+			- When n = 0, the function simple returns m + 1.
+
+		Recursive Cases:
+			- When n > 0 and m = 0, the function returns A(n - 1, m - 1).
+			- When n > 0 and m > 0, the function calls A( n - 1, A(n, m - 1) ).
+
+		Ackermann Function Simplified Expressions for Small `n` Values:
+		For small values of 'n', the Ackermann function has some simplified expressions. Below is the breakdown for specific values of 'n':
+
+			- A(0, n) = m + 1
+			- A(1, n) = m + 2
+			- A(2, n) = 2 * m + 3
+			- A(3, n) = 2^(m + 3) - 3
+			- A(4, n) = 2^(2^(m + 3)) - 3
+	*/
+
+	// simplified expressions for small `n` values
+	switch(n)
+	{
+		case 0: return m + 1; break;
+		case 1: return m + 2; break;
+		case 2: return 2 * m + 3; break;
+		case 3: return pow(2, m + 3) - 3; break;
+		case 4: return pow(2, pow(2, m + 3)) - 3; break;
+	}
+
+	// base conditions
+	if(m == 0)	return recursiveAckerman(n -= 1, m = 1);
+	else if(n > 0 && m > 0) return recursiveAckerman( n -= 1, recursiveAckerman(n, m -= 1) );
+}
