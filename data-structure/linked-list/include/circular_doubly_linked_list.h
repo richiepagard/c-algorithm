651560eb54adb6396cc9ec7bb4a5765508b41205
richie.p <mohammadpagard.dev@gmail.com>
2024-11-10 23:11:13 +0330

    improve&implement -Improve the linked list by implemented the Circular Doubly Linked List and test it in the main file and it worked well.

---
+++ data-structure/linked-list/include/circular_doubly_linked_list.h
@@ -0,0 +1,16 @@
+#ifndef CIRCULAR_DOUBLY_LINKED_LIST
+#define CIRCULAR_DOUBLY_LINKED_LIST
+
+typedef struct node_circular_doubly {
+	int data;
+	struct node_circular_doubly *next;
+	struct node_circular_doubly *previous;
+} NodeCircularDoubly;
+
+NodeCircularDoubly *initializeCircularDoubly(int value);
+void insertBeginCircularDoubly(NodeCircularDoubly **head, NodeCircularDoubly **last, int value);
+void insertLastCircularDoubly(NodeCircularDoubly **head, NodeCircularDoubly **last, int value);
+void deleteWholeListCircularDoubly(NodeCircularDoubly **head);
+void displayCircularDoubly(NodeCircularDoubly **head, NodeCircularDoubly **last);
+
+#endif // CIRCULAR_DOUBLY_LINKED_LIST
