51b5bcf1f4bca9ed8b3538e28511e2f0a8e16a12
richie <richiepagard@gmail.com>
2025-09-20 23:25:22 +0330

    refactor: Replace `camelCase` methods to `snake_case` in Valid Palindrome problem.

--- problems/valid-palindrome/main.c
+++ problems/valid-palindrome/main.c
@@ -3,12 +3,12 @@
 #include <stdbool.h>
 
 
-bool isPalindrome(char *s);
+bool is_palindrome(char *s);
 
 int main() {
-	bool is_palindrome = isPalindrome("A man, a plan, a canal: Panama");
+	bool isPalindrome = is_palindrome("A man, a plan, a canal: Panama");
 
-	if(is_palindrome) printf("The string is palindrome.\n");
+	if(isPalindrome) printf("The string is palindrome.\n");
 	else printf("The string isn't palindrome.\n");
 
 	// Problem link on Leetcode
@@ -18,7 +18,7 @@ int main() {
 }
 
 
-int stringLength(char *string) {
+int string_length(char *string) {
 	int counter = 0;
 
 	// Count characters until the null-terminate is reached
@@ -28,7 +28,7 @@ int stringLength(char *string) {
 	return counter;
 }
 
-void stringCopy(char *copied_str, char *main_str) {
+void string_copy(char *copied_str, char *main_str) {
 	int i = 0, j = 0;
 
 	// Copy only ascii letters (a-z, A-Z) one by one until the null-terminate is reached
@@ -54,20 +54,20 @@ void stringCopy(char *copied_str, char *main_str) {
 	printf("%s\n", copied_str);
 }
 
-bool isPalindrome(char *s) {
-	int string_length = stringLength(s);
+bool is_palindrome(char *s) {
+	int stringLength = string_length(s);
     int left, right;
 
 	// Allocate memory for a modifiable copy(+1 for null-terminate)
-	char *copy = malloc(string_length + 1);
+	char *copy = malloc(stringLength + 1);
 	if(!copy) return 0;
 
 	// Copy the input string to the buffer
-	stringCopy(copy, s);
+	string_copy(copy, s);
 
-	int copy_string_length = stringLength(copy);
+	int copy_strinLength = string_length(copy);
     left = 0;
-    right = copy_string_length - 1;
+    right = copy_strinLength - 1;
 
     while(left <= right)
     {
